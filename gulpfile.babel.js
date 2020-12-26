0b68e90824abfbcae321d14572d7c1b8ed777fb8
so0choi <simc2644@gmail.com>
2020-12-27 02:00:02 +0900

    add gulp-image lib

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,17 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import image from "gulp-image";
 
 const routes = {
     pug: {
         watch: "src/**/*.pug",
         src: "src/*.pug",
         dest: "build"
+    },
+    img: {
+        src: "src/img/*",
+        dest: "build/img"
     }
 };
 
@@ -18,6 +23,7 @@ const pug = () =>
       .pipe(gulp.dest(routes.pug.dest));
 
 
+
 const clean = () => del(["build"]);
 
 const webserver = () => gulp.src("build").pipe(ws({livereload: true, open: true}));
@@ -26,7 +32,9 @@ const watch = () => {
     gulp.watch(routes.pug.watch, pug);
 }
 
-const prepare = gulp.series([clean]);
+const img = () => gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
+
+const prepare = gulp.series([clean,img]);
 
 const assets = gulp.series([pug]);
 
